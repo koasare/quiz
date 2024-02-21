494366ba3a55ad4df2026cd0181f83e583f35520
Kwesi Asare <15335005+koasare@users.noreply.github.com>
2024-02-20 22:42:22 -0600

    quiz complete no time function

--- quiz.go
+++ quiz.go
@@ -5,7 +5,30 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
+
+// Type Definitions
+type Problems struct {
+	question string
+	answer   string
+}
+
+// Functions
+func parseLines(records [][]string) []Problems {
+	result := make([]Problems, len(records))
+	for i, record := range records {
+		result[i] = Problems{
+			question: record[0],
+			answer:   strings.TrimSpace(record[1]),
+		}
+	}
+	return result
+}
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
 
 func main() {
 	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format of 'question, answer'")
@@ -14,10 +37,27 @@ func main() {
 	file, err := os.Open(*csvFileName)
 	if err != nil {
 		fmt.Println(err)
+		exit(fmt.Sprintf("Failed to open CSV file: %s\n", *csvFileName))
 	}
 	reader := csv.NewReader(file)
-	records, _ := reader.ReadAll()
-
-	fmt.Println(records)
+	records, err := reader.ReadAll()
+	if err != nil {
+		exit("Failed to Parse File")
+	}
+	problems := parseLines(records)
+	//fmt.Println(problems)
+	correct_answer := 0
+	for i, problem := range problems {
+		fmt.Printf("Problem #%d: %s =  \n", i+1, problem.question)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == problem.answer {
+			correct_answer++
+		} else {
+			fmt.Println("Oops, wrong Answer!")
+			fmt.Println("The correct answer is:", problem.answer)
+		}
+	}
+	fmt.Printf("You scored %d out of %d questions", correct_answer, len(problems))
 
 }
